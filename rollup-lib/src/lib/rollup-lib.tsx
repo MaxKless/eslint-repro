b82ae8bcd95a15a73d028ffcc3ace46d41f832a2
Max Kless <maxk@nrwl.io>
2023-04-25 12:57:31 +0200

    use lib

--- rollup-lib/src/lib/rollup-lib.tsx
+++ rollup-lib/src/lib/rollup-lib.tsx
@@ -1,9 +1,11 @@
+import { utilsEsbuild } from '@v16-type-repro/utils-esbuild';
 import styles from './rollup-lib.module.css';
 
 /* eslint-disable-next-line */
 export interface RollupLibProps {}
 
 export function RollupLib(props: RollupLibProps) {
+  utilsEsbuild();
   return (
     <div className={styles['container']}>
       <h1>Welcome to RollupLib!</h1>
