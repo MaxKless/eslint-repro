b8f8b2c29d69aed1587440b7b1640f8110aa19e0
Max Kless <maxk@nrwl.io>
2023-04-25 12:42:30 +0200

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="v16-type-repro" />
+    </div>
+  );
+}
+
+export default App;
